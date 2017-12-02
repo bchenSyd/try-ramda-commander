83663513b1cdade3174d64dafef9307ed548523b
Bo Chen <bochen2014@yahoo.com>
2017-12-02 13:33:58 +1100

    update

--- src/printObject.js
+++ src/printObject.js
@@ -1,19 +1,60 @@
-const data = {
+function ProgressPlugin() {
+
+}
+function HtmlWebpackPlugin(options) {
+    this.options = options
+}
+function DefinePlugin(definitions) {
+    this.definitions = definitions;
+}
+const config = {
     entry: 'src/index.js',
     module: {
-        use: [{
-            loader: 'style-loader',
-            test: /\.css/
+        rules: [{
+            test: /\.css/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            "test": "/\\.(png|jpe?g|svg|woff2?|ttf|eot)$/",
+            "loader": "url-loader?limit=8000"
         }]
+    },
+    plugins: [
+        new ProgressPlugin(),
+        new HtmlWebpackPlugin({
+            "template": "/media/bochen2014/Work/__work/arc/public/index.html",
+            "filename": "index.html",
+            "hash": false,
+            "inject": true,
+        }),
+        new DefinePlugin({
+            "process.env.NODE_ENV": "\"development\"",
+            "process.env.PUBLIC_PATH": "\"\""
+        })
+    ],
+    resolve: {
+        exensions: ['.js', 'jsx', 'json']
     }
 }
 
 
+/* eslint-disable */
 function printFile(fileName, value) {
-    var path = require('path')
-    var fs = require('fs');
-    //Yes, because there's no canonical representation for a RegExp object in JSON. Thus, it's just an empty object.
+    const path = require('path');
+    const fs = require('fs');
+    const util = require('util');
+    // issue: JSON.stringify(/\.css/) print an empty object
+    // https://stackoverflow.com/a/12075970
+    // because there's no canonical representation for a RegExp object in JSON. Thus, it's just an empty object.
+
+
+    // solution 1: use util.inspect
+    //const result=util.inspect(config, { showHidden: false, depth: null });
+
+    // solution 2: 
     RegExp.prototype.toJSON = RegExp.prototype.toString;
+    value.plugins.forEach(p => {
+        p.__type = p.constructor.name;
+    });
     fs.writeFile(path.join(__dirname, fileName), JSON.stringify(value), function (err) {
         if (err) {
             return console.log(err);
@@ -22,4 +63,5 @@ function printFile(fileName, value) {
     });
 }
 
-printFile('webpack.config.json', data)
+printFile('webpack.config.json', config)
+  /* eslint-enable */
