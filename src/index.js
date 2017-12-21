a725f3c288a4a2be48a5b87d19569927b63a0133
Bo Chen <bo.chen@vodafone.com.au>
2017-12-21 12:19:05 +1100

    update

--- src/index.js
+++ src/index.js
@@ -1,75 +1,71 @@
 import R from 'ramda';
 
-const data = {
-    result: "SUCCESS",
-    interfaceVersion: "1.0.3",
-    requested: "10/17/2013 15:31:20",
-    lastUpdated: "10/16/2013 10:52:39",
-    tasks: [
-        {
-            id: 104, complete: false, priority: "high",
-            dueDate: "2013-11-29", username: "Scott",
-            title: "Do something", created: "9/22/2013"
-        },
-        {
-            id: 105, complete: false, priority: "medium",
-            dueDate: "2013-11-22", username: "Lena",
-            title: "Do something else", created: "9/22/2013"
-        },
-        {
-            id: 107, complete: true, priority: "high",
-            dueDate: "2013-11-22", username: "Mike",
-            title: "Fix the foo", created: "9/22/2013"
-        },
-        {
-            id: 108, complete: false, priority: "low",
-            dueDate: "2013-11-15", username: "Lena",
-            title: "Adjust the bar", created: "9/25/2013"
-        },
-        {
-            id: 110, complete: false, priority: "medium",
-            dueDate: "2013-11-15", username: "Scott",
-            title: "Rename everything", created: "10/2/2013"
-        },
-        {
-            id: 112, complete: true, priority: "high",
-            dueDate: "2013-11-27", username: "Lena",
-            title: "Alter all quuxes", created: "10/5/2013"
-        },
-        {
-            id: 113, complete: true, priority: "high",
-            dueDate: "2013-11-27", username: "Lena",
-            title: "Alter all quuxes", created: "10/5/2013"
-        }
-        // , ...
-    ]
-};
-function serverRequest() {
-    return new Promise((res, rej) => {
-        setTimeout(() => {
-            res(data);
-        }, 1000);
-    })
-}
+const data = [
+    {
+        id: 104, complete: false, priority: "high",
+        dueDate: "2013-11-29", username: "Scott",
+        title: "Do something", created: "9/22/2013"
+    },
+    {
+        id: 105, complete: false, priority: "medium",
+        dueDate: "2013-11-22", username: "Lena",
+        title: "Do something else", created: "9/22/2013"
+    },
+    {
+        id: 107, complete: true, priority: "high",
+        dueDate: "2013-11-22", username: "Mike",
+        title: "Fix the foo", created: "9/22/2013"
+    },
+    {
+        id: 108, complete: false, priority: "low",
+        dueDate: "2013-11-15", username: "Lena",
+        title: "Adjust the bar", created: "9/25/2013"
+    },
+    {
+        id: 110, complete: false, priority: "medium",
+        dueDate: "2013-11-15", username: "Scott",
+        title: "Rename everything", created: "10/2/2013"
+    },
+    {
+        id: 112, complete: true, priority: "high",
+        dueDate: "2013-11-27", username: "Lena",
+        title: "Alter all quuxes", created: "10/5/2013"
+    },
+    {
+        id: 113, complete: true, priority: "high",
+        dueDate: "2013-11-27", username: "Lena",
+        title: "Alter all quuxes", created: "10/5/2013"
+    }
+];
 
-function clientCode(userName) {
-    return serverRequest()
-        .then(R.prop('tasks'))
-        .then(R.filter(R.propEq('username', userName)))
-        .then(R.reject(R.propEq('complete', true)))
-        .then(R.map(R.pick(['id', 'dueDate', 'title', 'priority'])))
-        .then(R.sortBy(R.prop('dueDate')));
 
-}
+// R.compose   R.compose(Left <---- Right )(data);
+// The rightmost function may have any arity (have more than 1 para); the remaining functions must be unary.
 
-clientCode('Lena')
-    .then(result => {
-        console.log(result)
-    });
+// R.pipe   R.pipe(Left ------>  Right )(data);
+// The leftmost function may have any arity (have more than 1 para); the remaining functions must be unary.
+
+
+const userName = 'Lena';
+const byName = R.filter(R.where({ username: R.equals(userName) }))(data);
+const notCompleted = R.reject(R.propEq('complete', true))(byName);
+
+const pickProps = R.pick(['id', 'dueDate', 'title', 'priority']);
+// R.map  Takes a function and a functor (e.g array), applies the function to each of the functor's values,
+// and returns a functor of the same shape.
+const selected = R.map(pickProps)(notCompleted);
+
+// R.pluck is equivalent to R.map(R.prop(k), f); so it can only pluck ONE prop of its itmes
+const selected2 = R.pluck('title')(notCompleted);  // pluck = map(prick([ONE-PROP-NAME]))
+
+
+const upperCasePriority = R.over(R.lensProp('priority'), R.toUpper);
+
+const upperCased = R.map(upperCasePriority)(selected);
+
+R.sortBy(R.compose(R.negate, R.prop('id')))(upperCased);
+console.log(R.sortBy(R.compose(R.negate, R.prop('id')))(upperCased));
 
 export const start=()=>{
-    clientCode('Lena')
-    .then(result => {
-        console.log(result)
-    });
+   console.log('program started...')
 }
