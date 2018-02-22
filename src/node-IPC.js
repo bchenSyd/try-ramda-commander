4dc0ce44f8b7b9b7f995a76e72d09fa4cbdba501
Bo Chen <bo.chen@vodafone.com.au>
2018-02-22 14:58:46 +1100

    update

--- src/node-IPC.js
+++ src/node-IPC.js
@@ -3,31 +3,41 @@
 var childProcess = require('child_process');
 
 function execTests() {
-    const child = childProcess.spawn(
-      'yarn',
-      [ 'babel-node',
-        'src/index.js'
-      ], 
-      {
-        // programmatically get spwan output, you have to comment out below line
-        stdio: [0, 1, 2], //calling-process.stdin: 0, calling-process.stdout:1, calling-process.err:2
-      },
-    );
+    return new Promise((res, rej) => {
+        const child = childProcess.spawn(
+            'yarn',
+            [   'babel-node',
+                'src/index.js'
+            ],
+            {
+                // programmatically get spwan output, you have to comment out below line
+                stdio: [0, 1, 2], //calling-process.stdin: 0, calling-process.stdout:1, calling-process.err:2
+            },
+        );
 
-    // child.stdout.on('data', function(data){
-    //     console.log(`programmatically print child spawn stdout ${data}`)
-    // });
+        // child.stdout.on('data', function(data){
+        //     console.log(`programmatically print child spawn stdout ${data}`)
+        // });
 
-    child.on('close', (exitCode) => {
-      console.log('closed! exit code:', exitCode);
-      process.exit(exitCode);
+        child.on('close', (exitCode) => {
+            console.log('closed! exit code:', exitCode);
+            res(exitCode);
+        });
+
+        child.on('error', (childErr) => {
+            console.log(childErr);
+            rej(childErr);
+        });
     });
-
-    child.on('error', (childErr) => {
-      console.log(childErr);
-     // throw childErr;
-    });
-  }
+}
 
 
-  execTests();
+execTests()
+.then(exitCode=>{
+    console.log('child exit code: ' + exitCode)
+})
+.catch(err=>{
+    console.error(err)
+});
+
+process.exit(0); // here is how you control process return code
