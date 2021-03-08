a78a1a51ee0f81622e4dd1389826ac2a0f985043
Bo Chen <bochen2014@yahoo.com>
2021-03-08 11:17:43 +1100

    Update janine.js

--- src/janine.js
+++ src/janine.js
@@ -1,4 +1,7 @@
 var execSync = require('child_process').execSync;
 
+
+// git 2.2+
+// git branch --show-current
 let branch = execSync(`git rev-parse --abbrev-ref HEAD`).toString('utf8');
 console.log(branch)
